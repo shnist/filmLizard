01b0b193995c90c9b3e06bd9276759997a574f80
Aaron Faber <shnist@btinternet.com>
2011-12-26 22:39:36 +0000

    added basic styling to search.php

--- assets/scripts/php/search.php
+++ assets/scripts/php/search.php
@@ -7,16 +7,30 @@
         // start a new connection to the database
         $databaseConnection = new database("localhost", "root", "", "films");
     } 
+?>
+<!DOCTYPE html>
 
+<html>
+<head>
+    <title>Aaron's Film Collection</title>
+    <link href="/assets/styles/common.css" rel="stylesheet">
+</head>
+
+<body>
+    <div id="page">
+<?php
     // if someone has search by title
     if($_POST['film-search'] !== ''){
         $filmTitle = $_POST['film-search'];
         $titleResult = $databaseConnection->searchByTitle($filmTitle);
-
+       
         if ($titleResult !== null) {
             echo "<ul>";
             echo "<li>Title: ".$titleResult->title."</li>";
             echo "<li>Rating: ".$titleResult->rating."</li>";
+            echo "<li>Certificate: ".$titleResult->certificate."</li>";
+            echo "<li>Release Year: ".$titleResult->releaseDate."</li>";
+            echo "<li><img src='".urldecode($titleResult->poster)."' alt='".$titleResult->title."'></li>";
             echo "</ul>";
         }
 
@@ -24,3 +38,7 @@
         unset($databaseConnection);
     }
 ?>
+    <a href="/index.php">Search another film</a>
+    </div>
+</body>
+</html>
--- statements.sql
+++ statements.sql
@@ -3,7 +3,7 @@ use films;
 
 # table for films
 create table film (id int(3) primary key not null auto_increment,
-title varchar(30) not null,
+title varchar(40) not null,
 certificate varchar(5),
 releaseDate varchar(4),
 poster varchar(200),
@@ -49,3 +49,24 @@ insert into film (title) values ("Collateral");
 insert into film (title) values ("War of the Worlds");
 insert into film (title) values ("No Country For Old Men");
 insert into film (title) values ("Star Trek");
+insert into film (title) values ("Rare Exports: A Christmas Tale");
+insert into film (title) values ("The Lives of Others");
+insert into film (title) values ("Crouching Tiger Hidden Dragon");
+insert into film (title) values ("The Girl With The Dragon Tattoo");
+insert into film (title) values ("300");
+insert into film (title) values ("Dejavu");
+insert into film (title) values ("Die Hard");
+insert into film (title) values ("Hancock");
+insert into film (title) values ("Black Hawk Down");
+insert into film (title) values ("Blade Runner");
+insert into film (title) values ("Superman Returns");
+insert into film (title) values ("Alien vs Predator");
+insert into film (title) values ("Ace Ventura Pet Detective");
+insert into film (title) values ("Jumanji");
+insert into film (title) values ("Spider Man 2");
+insert into film (title) values ("Big Fish");
+insert into film (title) values ("The Aviator");
+insert into film (title) values ("Australia");
+insert into film (title) values ("The Butterfly Effect");
+insert into film (title) values ("Cast Away");
+insert into film (title) values ("Borat");
