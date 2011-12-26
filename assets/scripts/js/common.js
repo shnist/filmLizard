fcd7991e8599e09f5d477d6ecdc44ed17ae72bc9
Aaron Faber <shnist@btinternet.com>
2011-12-26 21:51:17 +0000

    ui select behaviour added

--- assets/scripts/js/common.js
+++ assets/scripts/js/common.js
@@ -2,7 +2,7 @@ var api = window.api || {},
     database = window.database || {};
 
 api = {
-    connect : function (search) {
+    searchFilms : function (search) {
         // add loader
         $('.imdb').append('<div id="loader"> </div>');
         $.ajax({
@@ -12,32 +12,36 @@ api = {
                 "q" : search
             },
             dataType : "JSONP",
-            success: api.success,
+            success: function (result) {
+                // remove loader when results have returned
+                $('#loader').remove();
+                
+                if ($('#film-update').length){
+                api.updateFilm(result);
+                }
+                //if ($('#genre-update').length) {
+                //    imdb.updateGenre(result);
+                //}
+            },
             timeout : 4000,
-            error : api.error
-        
+            error : function () {
+                // remove loader when results have returned
+                $('#loader').remove();        
+                $('.imdb').append("<p> Request has timed out </p>");
+            }
         });
     },
-    success : function (result) {
-        // remove loader when results have returned
-        $('#loader').remove();
-
-        if ($('#film-update').length){
-            api.updateFilm(result);
-        }
-        //if ($('#genre-update').length) {
-        //    imdb.updateGenre(result);
-        //}
-    
-    },
-    error : function () {
-        // remove loader when results have returned
-        $('#loader').remove();        
-        $('.imdb').append("<p> Request has timed out </p>");
-    },
+    filmDetails : function () {
+        
+        
+    }
     updateFilm : function (result) {
-        console.log(result.movies);
-        var htmlString = '<ul class="film-search-results">';
+        // remove previous results
+        if ($('.film-search-results').length === true){
+            $('.film-search-results').remove();
+            $('.error').remove();
+        }
+        var htmlString = '<form action="#" method="post" class="film-search-results"><ul>';
         // populate the populate database form with retrieved values
         for (var i = 0; i < result.movies.length; i = i + 1){
             htmlString = htmlString +  '<li><a href="#">'
@@ -46,15 +50,45 @@ api = {
             + '<p class="year">' + result.movies[i].year + '</p>'
             + '</a></li>'
         }
-        htmlString = htmlString + "</ul>";
+        htmlString = htmlString + '</ul><input type="submit" name="submit" value="populate form"></form>';
         $('#film-update').after(htmlString);
+        api.ui.selectFilm();
+        api.ui.populateForm(); 
     },
     updateGenre : function (result) {
         console.log(result);
         var genres = result.Genres;
         
         $('#genres').val(genres);
+    },
+    ui : {
+        selectFilm : function () {
+            $('.film-search-results a').click(function (e) {
+                e.preventDefault();
+                $('.film-search-results a').removeClass('selected');
+                $(this).addClass('selected');
+                $('.error').remove();
+            });
+        },
+        populateForm : function () {
+            $('.film-search-results').submit(function (e) {
+                e.preventDefault();
+                // first error checking
+                if ($('.selected', '.film-search-results').length === 0){
+                    $('#film-update').append('<p class="error">Nothing was selected</p>');
+                } else {
+                    api.filmDetails();
+                    
+                    $('#certificate').val();
+                    $('#release-date').val();
+                    $('#rating').val();
+                    $('#poster').val();
+                }
+            
+            });
+        }
     }
+
 };
 
 database = {
