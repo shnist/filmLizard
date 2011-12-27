358fe308438b8aa7f1a10e842ee9d07c36042571
Aaron Faber <shnist@btinternet.com>
2011-12-27 22:48:25 +0000

    modified html structure so that nav sits more nicely on the page

--- filmUpdate.php
+++ filmUpdate.php
@@ -23,70 +23,72 @@
                 <li>
                     <a href="filmUpdate.php">Update Database</a>
                 </li>
-            </ul>            
-            <h1>Database</h1>
-            <p>
-                This page is used for populating the database.
-            </p>
-            <p>
-                Below is a list of all the films that have not
-                been populate with data yet. Click on one of them to populate the form, which can then
-                be used to search for data on a film.
-            </p>
-            <div class="results">
-                <h2> Unpopulated data </h2>
-<?php
-        $query = "select * from film where certificate is null";
-        $results = $databaseConnection->retrieveUnpopulated($query);
-        $length = sizeof($results);
-        echo "<ul>";
-         for ($i = 0; $i < $length; $i++) {
-            echo "<li><a href='#'>".$results[$i]["title"]."</a></li>";
-         }
-        echo "</ul>";
-?>
+            </ul>
+            <div id="content">
+                <h1>Database</h1>
+                <p>
+                    This page is used for populating the database.
+                </p>
+                <p>
+                    Below is a list of all the films that have not
+                    been populate with data yet. Click on one of them to populate the form, which can then
+                    be used to search for data on a film.
+                </p>
+                <div class="results">
+                    <h2> Unpopulated data </h2>
+    <?php
+            $query = "select * from film where certificate is null";
+            $results = $databaseConnection->retrieveUnpopulated($query);
+            $length = sizeof($results);
+            echo "<ul>";
+             for ($i = 0; $i < $length; $i++) {
+                echo "<li><a href='#'>".$results[$i]["title"]."</a></li>";
+             }
+            echo "</ul>";
+    ?>
+                </div>
+                <form action="#" method="POST" class="imdb" id="film-update">
+                    <fieldset>
+                        <legend> IMDB data search for films </legend>
+                        <label for="film-search">Search for this film on IMDB: </label>
+                        <input type="text" name="film-search" id="film-search">
+                        <input type="submit" value="search for film">
+                    </fieldset>
+                </form>
+                <form action="/assets/scripts/php/populateDatabase.php" method="POST">
+                    <fieldset>
+                        <legend> Populate the Database with the film </legend>
+                        <ul>
+                            <li>
+                                <label for="id">ID from database </label>
+                                <input type="text" name="id" id="id">
+                            </li>
+                            <li>
+                                <label for="certificate">Certificate</label>
+                                <input type="text" name="certificate" id="certificate">
+                            </li>
+                            <li>
+                                <label for="release-date">Release Date </label>
+                                <input type="text" name="release-date" id="release-date">
+                            </li>
+                            <li>
+                                <label for="rating">Rating </label>
+                                <input type="text" name="rating" id="rating">
+                            </li>
+                            <li>
+                                <label for="poster">Poster</label>
+                                <input type="text" name="poster" id="poster">
+                            </li>
+                            <li>
+                                <label for="genres">Genres</label>
+                                <textarea name="genres" id="genres"></textarea>
+                            </li>
+                        </ul>
+                        <input type="submit" value="populate database" name="submit">
+                    </fieldset>
+                    
+                </form>
             </div>
-            <form action="#" method="POST" class="imdb" id="film-update">
-                <fieldset>
-                    <legend> IMDB data search for films </legend>
-                    <label for="film-search">Search for this film on IMDB: </label>
-                    <input type="text" name="film-search" id="film-search">
-                    <input type="submit" value="search for film">
-                </fieldset>
-            </form>
-            <form action="/assets/scripts/php/populateDatabase.php" method="POST">
-                <fieldset>
-                    <legend> Populate the Database with the film </legend>
-                    <ul>
-                        <li>
-                            <label for="id">ID from database </label>
-                            <input type="text" name="id" id="id">
-                        </li>
-                        <li>
-                            <label for="certificate">Certificate</label>
-                            <input type="text" name="certificate" id="certificate">
-                        </li>
-                        <li>
-                            <label for="release-date">Release Date </label>
-                            <input type="text" name="release-date" id="release-date">
-                        </li>
-                        <li>
-                            <label for="rating">Rating </label>
-                            <input type="text" name="rating" id="rating">
-                        </li>
-                        <li>
-                            <label for="poster">Poster</label>
-                            <input type="text" name="poster" id="poster">
-                        </li>
-                        <li>
-                            <label for="genres">Genres</label>
-                            <textarea name="genres" id="genres"></textarea>
-                        </li>
-                    </ul>
-                    <input type="submit" value="populate database" name="submit">
-                </fieldset>
-                
-            </form>
         </div>
         <!-- javascript files -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
--- index.php
+++ index.php
@@ -16,68 +16,70 @@
                 <a href="filmUpdate.php">Update Database</a>
             </li>
         </ul>
-        <h1> Aaron's Film Collection </h1>
-        <form action="/assets/scripts/php/search.php" method="POST">
-            <fieldset>
-                <legend>Search for a film </legend>
-                <label for="film-search">Search by title</label>
-                <input type="text" name="film-search" id="film-search"> 
-            </fieldset>
-            <input type="submit" value="Search" name="submit">
-        </form>
-        <h2> Categories </h2>
-        <form action="/assets/scripts/php/categories.php" method="POST">
-            <fieldset>
-                <legend>Search by genre </legend>
-                <label for="genre-search"> Search by genre </label>
-                <select name="genre" id="genre-search">
-                    <option value="select"> Select an option </option>
-                    <option value="action">Action </option>
-                    <option value="romance"> Romance </option>
-                    <option value="comedy"> Comedy </option>
-                    <option value="thriller"> Thriller </option>
-                    <option value="horror"> Horror </option>
-                    <option value="disney"> Disney </option>
-                    <option value="sci-fi"> Sci-Fi </option>
-                </select>
-            </fieldset>
-            <fieldset>
-                <legend>Search by rating </legend>
-                <select name="rating" id="rating">
-                    <option value="select">Select an option</option>
-                    <option value="100">100</option>
-                    <option value="90">90</option>
-                    <option value="80">80</option>
-                    <option value="70">70</option>
-                    <option value="60">60</option>
-                    <option value="50">50</option>
-                    <option value="40">40</option>
-                    <option value="30">30</option>
-                    <option value="20">20</option>
-                    <option value="10">10</option>
-                </select>
-            </fieldset>
-            <fieldset>
-                <legend>Search by release date </legend>
-                <select name="date" id="date">
-                    <option value="select">Select an option</option>
-                    <option value="2012">2012</option>
-                    <option value="2011">2011</option>
-                    <option value="2010">2010</option>
-                    <option value="2009">2009</option>
-                    <option value="2008">2008</option>
-                    <option value="2007">2007</option>
-                    <option value="2006">2006</option>
-                    <option value="2005">2005</option>
-                    <option value="2004">2004</option>
-                    <option value="2003">2003</option>
-                    <option value="2002">2002</option>
-                    <option value="2001">2001</option>
-                    <option value="2000">2000</option>
-                </select>
-            </fieldset>
-            <input type="submit" value="Search" name="submit" class="submit">
-        </form>
+        <div id="content">
+            <h1> Aaron's Film Collection </h1>
+            <form action="/assets/scripts/php/search.php" method="POST">
+                <fieldset>
+                    <legend>Search for a film </legend>
+                    <label for="film-search">Search by title</label>
+                    <input type="text" name="film-search" id="film-search"> 
+                </fieldset>
+                <input type="submit" value="Search" name="submit">
+            </form>
+            <h2> Categories </h2>
+            <form action="/assets/scripts/php/categories.php" method="POST">
+                <fieldset>
+                    <legend>Search by genre </legend>
+                    <label for="genre-search"> Search by genre </label>
+                    <select name="genre" id="genre-search">
+                        <option value="select"> Select an option </option>
+                        <option value="action">Action </option>
+                        <option value="romance"> Romance </option>
+                        <option value="comedy"> Comedy </option>
+                        <option value="thriller"> Thriller </option>
+                        <option value="horror"> Horror </option>
+                        <option value="disney"> Disney </option>
+                        <option value="sci-fi"> Sci-Fi </option>
+                    </select>
+                </fieldset>
+                <fieldset>
+                    <legend>Search by rating </legend>
+                    <select name="rating" id="rating">
+                        <option value="select">Select an option</option>
+                        <option value="100">100</option>
+                        <option value="90">90</option>
+                        <option value="80">80</option>
+                        <option value="70">70</option>
+                        <option value="60">60</option>
+                        <option value="50">50</option>
+                        <option value="40">40</option>
+                        <option value="30">30</option>
+                        <option value="20">20</option>
+                        <option value="10">10</option>
+                    </select>
+                </fieldset>
+                <fieldset>
+                    <legend>Search by release date </legend>
+                    <select name="date" id="date">
+                        <option value="select">Select an option</option>
+                        <option value="2012">2012</option>
+                        <option value="2011">2011</option>
+                        <option value="2010">2010</option>
+                        <option value="2009">2009</option>
+                        <option value="2008">2008</option>
+                        <option value="2007">2007</option>
+                        <option value="2006">2006</option>
+                        <option value="2005">2005</option>
+                        <option value="2004">2004</option>
+                        <option value="2003">2003</option>
+                        <option value="2002">2002</option>
+                        <option value="2001">2001</option>
+                        <option value="2000">2000</option>
+                    </select>
+                </fieldset>
+                <input type="submit" value="Search" name="submit" class="submit">
+            </form>
+        </div>
     </div>
 </body>
 </html>
