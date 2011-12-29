fcf47f2b867cb847baa4e4cab8072bfc49543892
Aaron Faber <shnist@btinternet.com>
2011-12-29 15:23:33 +0000

    removed the automatic insertion films

--- statements.sql
+++ statements.sql
@@ -28,32 +28,3 @@ create table actorFilm (actorId int(2),
 filmId int(2),
 foreign key (actorId) references actor(id),
 foreign key (filmId) references film(id)) engine=InnoDB;
-
-insert into film (title) values ("Taken");
-insert into film (title) values ("The Last Samurai");
-insert into film (title) values ("500 Days of Summer");
-insert into film (title) values ("Collateral");
-insert into film (title) values ("War of the Worlds");
-insert into film (title) values ("No Country For Old Men");
-insert into film (title) values ("Star Trek");
-insert into film (title) values ("Rare Exports: A Christmas Tale");
-insert into film (title) values ("The Lives of Others");
-insert into film (title) values ("Crouching Tiger Hidden Dragon");
-insert into film (title) values ("The Girl With The Dragon Tattoo");
-insert into film (title) values ("300");
-insert into film (title) values ("Dejavu");
-insert into film (title) values ("Die Hard");
-insert into film (title) values ("Hancock");
-insert into film (title) values ("Black Hawk Down");
-insert into film (title) values ("Blade Runner");
-insert into film (title) values ("Superman Returns");
-insert into film (title) values ("Alien vs Predator");
-insert into film (title) values ("Ace Ventura Pet Detective");
-insert into film (title) values ("Jumanji");
-insert into film (title) values ("Spider Man 2");
-insert into film (title) values ("Big Fish");
-insert into film (title) values ("The Aviator");
-insert into film (title) values ("Australia");
-insert into film (title) values ("The Butterfly Effect");
-insert into film (title) values ("Cast Away");
-insert into film (title) values ("Borat");
