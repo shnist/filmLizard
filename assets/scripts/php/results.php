954ae0cc9c44c2c29e1131de6ebd98fc597d5164
Aaron Faber <shnist@btinternet.com>
2011-12-29 16:52:57 +0000

    removing curl class as the methods were not working properly

--- assets/scripts/php/results.php
+++ assets/scripts/php/results.php
@@ -5,6 +5,7 @@
         <link href="/assets/styles/common.css" rel="stylesheet">
     </head>
     <body>
+        <div id="page">
 
 <?php
 
@@ -28,11 +29,46 @@ if (isset($_POST['submit'])){
         
         // play with the data!
         $movies = $search_results->movies;
+        //var_dump($movies);
         echo '<ul>';
         foreach ($movies as $movie) {
           echo '<li><a href="' . $movie->links->alternate . '">' . $movie->title . " (" . $movie->year . ")</a></li>";
         }
         echo '</ul>';
+        
+
+//// construct the query with our apikey and the query we want to make
+//$endpoint = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=' . $apikey . '&q=' . $q;
+//
+//// setup curl to make a call to the endpoint
+//$session = curl_init($endpoint);
+//
+//// indicates that we want the response back
+//curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+//
+//// exec curl and get the data back
+//$data = curl_exec($session);
+//
+//// remember to close the curl session once we are finished retrieveing the data
+//curl_close($session);
+//
+//// decode the json data to make it easier to parse the php
+//$search_results = json_decode($data);
+//if ($search_results === NULL) die('Error parsing json');
+//
+//// play with the data!
+//$movies = $search_results->movies;
+//echo '<ul>';
+//foreach ($movies as $movie) {
+//  echo '<li><a href="' . $movie->links->alternate . '">' . $movie->title . " (" . $movie->year . ")</a></li>";
+//}
+//echo '</ul>';
+
+
+
+        
+        
+        
         echo "<a href='/filmUpdate.php'>New search</a>";
     } elseif ($_POST['film-search'] === '') {
         echo "<p> You did not search for anything</p>";
@@ -42,19 +78,8 @@ if (isset($_POST['submit'])){
         echo "<a href='/filmUpdate.php'>New search</a>";
     }
 }
-    //// setup curl to make a call to the endpoint
-    //$session = curl_init($endpoint);
-    //
-    //// indicates that we want the response back
-    //curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-    //
-    //// exec curl and get the data back
-    //$data = curl_exec($session);
-    //
-    //// remember to close the curl session once we are finished retrieveing the data
-    //curl_close($session);
     
 ?>
-
+        </div>
     </body>
 </html>
