2ceee9b562385d9feeacea6043da5535f23b9263
Aaron Faber <shnist@btinternet.com>
2011-12-30 12:41:58 +0000

    added explanatory copy to the index page

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 
 <html>
 <head>
-    <title>Film Lizard</title>
-    <link href="/assets/styles/common.css" rel="stylesheet">
+    <?php
+        include '/htmlTemplates/blocks/b_0.0_head.html';
+    ?>
 </head>
-
 <body> 
     <div id="page">
         <?php
@@ -24,7 +24,18 @@
                     <legend>Search from random film</legend>
                     <input type="submit" value="i'm feeling lucky" name="random">
                 </fieldset>
-            </form>
+            </form>            
+            <h2> What is Film Lizard? </h2>
+            <p>
+                Film Lizard is a film library that enables you to keep a track of your movie collection
+                online.
+            </p>
+            <p>
+                Ever felt like watching a film, but weren't sure what to choose? Fancy watching a
+                film with Tom Hanks in, but unsure which one? With Film Lizard you effortlessly browse
+                through your collection by Genre and Actor. If you're not particularly fussed, why not
+                try the "I'm feeling lucky" option? This will select a random film from your collection.
+            </p>
             <h2> Categories </h2>
             <form action="/assets/scripts/php/categories.php" method="POST">
                 <fieldset>
