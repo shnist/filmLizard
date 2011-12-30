08f2e00742ec4a54b8820dcb6fbde554e644a5e4
Aaron Faber <shnist@btinternet.com>
2011-12-30 20:10:51 +0000

    placeholder support added for internet explorer

--- assets/scripts/js/common.js
+++ assets/scripts/js/common.js
@@ -34,7 +34,21 @@ common = {
     /* add placeholder support */
     placeHolder : function (){
         if(!$.support.placeholder) {
-            alert('placeholder not supported');
+            var active = document.activeElement;
+            $(':text').focus(function () {
+                    if ($(this).attr('placeholder') != '' && $(this).val() == $(this).attr('placeholder')) {
+                            $(this).val('').removeClass('hasPlaceholder');
+                    }
+            }).blur(function () {
+                    if ($(this).attr('placeholder') != '' && ($(this).val() == '' || $(this).val() == $(this).attr('placeholder'))) {
+                            $(this).val($(this).attr('placeholder')).addClass('hasPlaceholder');
+                    }
+            });
+            $(':text').blur();
+            $(active).focus();
+            $('form').submit(function () {
+                    $(this).find('.hasPlaceholder').each(function() { $(this).val(''); });
+            });
         }
     }
 }
